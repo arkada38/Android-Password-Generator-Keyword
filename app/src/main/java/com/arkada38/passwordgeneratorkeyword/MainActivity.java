8c0f309c1fdceac12e97c5d3c94e97689de71d2f
Arkadiy Kuznetsov <arkada38@outlook.com>
2016-10-28 21:15:34 +0700

    MVVM added

--- app/src/main/java/com/arkada38/passwordgeneratorkeyword/MainActivity.java
+++ app/src/main/java/com/arkada38/passwordgeneratorkeyword/MainActivity.java
@@ -1,13 +1,25 @@
 package com.arkada38.passwordgeneratorkeyword;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.arkada38.passwordgeneratorkeyword.databinding.ActivityMainBinding;
+
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        GeneratorForm generatorForm = null;
+        try {
+            generatorForm = new GeneratorForm("amazon", "horse", 12, true);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        binding.setGeneratorForm(generatorForm);
     }
 }
